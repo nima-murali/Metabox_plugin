91078253b132981e005e7ed8a34670951e1fc007
Nima <nimamuralis@gmail.com>
2021-10-08 15:53:27 +0530

    added nonce and sanitize properties

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <label for="author-name-input">Author's Name</label>
 <input type="text" name="author-name-input" id="author-name-input" value="<?php $id = get_the_ID(); $input_value = get_post_meta( $id, 'author-name-input', true ); echo $input_value ?>">
-<label for="author-enable-check">Apply</label>
+<label for="meta-box-check">Apply</label>
 <?php
 	$checkbox_value = get_post_meta( $id, 'author-enable-check', true );
 	if($checkbox_value == 'True'){ ?>
@@ -11,7 +11,9 @@
 		<input type="checkbox" id="author-enable-check" name="author-enable-check" value="True">
 	<?php
 	}
-	?>
+
+	wp_nonce_field( 'save_author_name', 'author_name_nonce_field' ); ?>
+
 
 	
 
--- metabox.php
+++ metabox.php
@@ -16,56 +16,37 @@ class Wpmetabox{
 	}
 
 	public function wp_add_author_name(){
-		add_meta_box( 'author-details-id', 'custom_metabox',array($this, 'wp_add_author_form'), 'post', 'side', 'high');	
+		add_meta_box( 'author-details-id', 'Display author details',array($this, 'wp_add_author_form'), 'post', 'side', 'high');																							// creating custom meta box which are only applied to posts
 	}
 
 	public function wp_add_author_form($post){
-		include plugin_dir_path(__FILE__).'form.php';
+		include plugin_dir_path(__FILE__).'form.php';		// includes html file
 	}
+
 	public function wp_author_name_save( $post_id ){
-		#if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
 		$inputs = [
         'author-name-input',
         'author-enable-check',
     	];
     	foreach ( $inputs as $input ) {
-    		#if ( array_key_exists( $input, $_POST ) ) {			// checks whether $input is 															present in the corresponding array
-    		if(isset($_POST)){
-            	update_post_meta( $post_id, $input,$_POST[$input]);
-            	#write_log($abc);
-        	}
-        
+    		if (! isset( $_POST['author_name_nonce_field'] )|| ! wp_verify_nonce( $_POST['author_name_nonce_field'], 'save_author_name' ))
+    		{
+   				wp_nonce_ays( '' );
+			} 
+			else{
+				if(isset($_POST['author-enable-check'])){
+            		update_post_meta( $post_id, $input,$_POST[$input]);
+        		}
+        		else{
+        			update_post_meta( $post_id, $input,sanitize_text_field( $_POST[$input] ));
+        		}
+			}			
+    		
     	}
-		#$this->wp_meta_box_get();     				// php oop method calling
-
 	}
+
 	public function wp_author_name_get($content){
-		/*$id = get_the_ID();							// Function used to get the post id of each post
-		$x = get_post_meta( $id, 'meta-box-input', true );
-		if(isset($_POST['meta-box-check'])){
-			#write_log($x);
-			echo "";	
-		
-
-
-
-		$postype = get_post_type();
-		if ( $postype == 'page' ) {
-        // Do something to $content
-			if(isset($_POST['meta-box-check'])){
-				$id = get_the_ID();
-				$x = get_post_meta( $id, 'meta-box-input', true );
-        		#return $content;
-        		$GLOBALS['a'] = $content;
-      
-        	}
-        	return $GLOBALS['a'];
-    	}
-    	else{
-    		return $content;
-    	}
-		*/
-    	$id = get_the_ID();
+    	$id = get_the_ID();								// taking postid
     	$check_box_value = get_post_meta( $id, 'author-enable-check', true );
 		if($check_box_value == 'True'){
 			$author_name = get_post_meta( $id, 'author-name-input', true );
@@ -74,20 +55,7 @@ class Wpmetabox{
 		else{
 			return $content;
 		}
-		#$postype = get_post_type();
-		#if ( $postype == 'page' ) {
-			
-			#$x = get_post_meta( $id, 'meta-box-input', true );
-			#return $x.$content;
-			#$a = $x.$content;
-			#return $a;
-		#}
-		#else{
-		#	return $content;
-		#}
 	}
-
-
 }
 
 
@@ -100,85 +68,3 @@ $objcheckbox	= new Wpmetabox();
 
 
 
-
-/*
-
-add_action( 'add_meta_boxes', 'wp_add_metabox' );
-add_action( 'save_post', 'wp_meta_box_save' );
-function wp_add_metabox(){
-	add_meta_box( 'metabox-id', 'custom_metabox','wp_custom_checkbox', 'page', 'side', 'high');	
-}
-
-function wp_custom_checkbox($post){
-	#echo "Hai";
-	#$content= '<label for="meta-box-input">Authors Name</label>\r\n';
-	#$content .= '<input type="text" name="meta-box-input" id="meta-box-input"/>';
-	
-	#return $content;
-	#$values	 	= get_post_custom($post->ID);
-	#$text		= isset($values['meta-box-input']) ? $values['meta-box-input'] : '';
-	#$selected	= isset($values['meta-box-select']) ? esc_attr($values['meta-box-select'][0]) : '';
-	#$check 		= isset($values['meta-box-check']) ? esc_attr($values['meta-box-check'][0]) : '';
-	#wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
-	include plugin_dir_path(__FILE__).'form.php';
-}
-
-function wp_meta_box_save( $post_id )
-{
-    $inputs = [
-        'meta-box-input',
-    ];
-    foreach ( $inputs as $input ) {
-    	
-    	if ( array_key_exists( $input, $_POST ) ) {
-            $abc = update_post_meta( $post_id, $input,$_POST[$input]);
-            write_log($abc);
-        }
-        
-    }
-	wp_meta_box_get();    
-
-}
-
-function wp_meta_box_get(){
-	$id = get_the_ID();
-	$x = get_post_meta( $id, 'meta-box-input', true );
-	if(isset($_POST['meta-box-check'])){
-		write_log($x);	
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if (!function_exists('write_log')) {
-	function write_log ( $log )  {
-		if ( true === WP_DEBUG ) {
-			if ( is_array( $log ) || is_object( $log ) ) {
-				error_log( print_r( $log, true ) );
-			} else {
-				error_log( $log );
-			}
-		}
-	}
-}
-
-*/
