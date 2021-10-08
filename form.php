995600bbeffa6514070ea1643e3e697313654fae
Nima <nimamuralis@gmail.com>
2021-10-08 14:42:27 +0530

    display author name if checkbox marked

---
+++ form.php
@@ -0,0 +1,18 @@
+<label for="author-name-input">Author's Name</label>
+<input type="text" name="author-name-input" id="author-name-input" value="<?php $id = get_the_ID(); $input_value = get_post_meta( $id, 'author-name-input', true ); echo $input_value ?>">
+<label for="author-enable-check">Apply</label>
+<?php
+	$checkbox_value = get_post_meta( $id, 'author-enable-check', true );
+	if($checkbox_value == 'True'){ ?>
+		<input type="checkbox" id="author-enable-check" name="author-enable-check" value="True" checked>
+	<?php
+	}
+	else{ ?>
+		<input type="checkbox" id="author-enable-check" name="author-enable-check" value="True">
+	<?php
+	}
+	?>
+
+	
+
+
